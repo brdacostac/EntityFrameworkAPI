4ccfe4642add97b587ba7d494adf84775fd4380b
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-02-02 15:21:25 +0100

    Teste Unitaire champion, methode Get

---
+++ Sources/TestChampion/ChampionControllerTest.cs
@@ -0,0 +1,32 @@
+using Api.Controllers;
+using ClientApi;
+using DTOLol;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using StubLib;
+
+namespace TestChampion
+{
+    [TestClass]
+    public class ChampionControllerTest
+    {
+        [TestMethod]
+        public async Task TestGetChampion()
+        {
+            IDataManager stubData = new StubData();
+            ChampionController controller = new ChampionController(stubData);
+            var championResult = await controller.GetAll();
+
+            var objectValue =  championResult as ObjectResult;
+            Assert.IsNotNull(objectValue);
+
+            var valeur = objectValue.Value as IEnumerable<DTOChampion>;
+            Assert.IsNotNull(valeur);
+
+            var expected = valeur.Count();
+            var actual = stubData.ChampionsMgr.GetNbItems().Result;
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
