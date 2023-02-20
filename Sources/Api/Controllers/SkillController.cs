3c9ee75ac21ea6174251732c84180265abeb9b8f
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-02-20 18:14:35 +0100

    SkillController debut, mais il faut faire le skill mgr d'abord

---
+++ Sources/Api/Controllers/SkillController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillController : ControllerBase
+    {
+        private readonly IDataManager _dataManager;
+        public SkillController(IDataManager dataManger)
+        {
+            _dataManager = dataManger;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                IEnumerable<Champion> skillList = await _dataManager.SkillsMgr.GetItems(0, await _dataManager.SkillsMgr.GetNbItems());
+                if (skillList.Count() == 0) return StatusCode((int)HttpStatusCode.NoContent);
+                return StatusCode((int)HttpStatusCode.OK, skillList.Select(e => e.ToDto()));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, FactoryMessage.MessageCreate("Une erreur est survenue lors de la récupération des champions"));
+            }
+        }
+
+        // GET: SkillController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SkillController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SkillController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SkillController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SkillController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SkillController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SkillController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
