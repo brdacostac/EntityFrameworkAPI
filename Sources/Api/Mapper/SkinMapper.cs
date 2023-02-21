3487d1990037610fe013c56861e3cff74778e92d
Noan07 <84435602+Noan07@users.noreply.github.com>
2023-02-21 13:13:19 +0100

    controller, mapper, dto, modif du stubSkin Problème avec méthode put post

---
+++ Sources/Api/Mapper/SkinMapper.cs
@@ -0,0 +1,27 @@
+﻿using DTOLol;
+using Model;
+
+namespace Api.Mapper
+{
+    public static class SkinMapper
+    {
+        public static DTOSkin ToDto(this Skin skin)
+        {
+            DTOSkin dTOSkin = new DTOSkin()
+            {
+                Name = skin.Name,
+                Champion = skin.Champion.ToDto(),
+                Description = skin.Description,
+                Price = skin.Price, 
+                Icon = skin.Icon,
+                Image = skin.Image.Base64
+            };
+            return dTOSkin;
+        }
+
+        public static Skin ToSkin(this DTOSkin dTOSkin)
+        {
+            return new Skin(dTOSkin.Name, dTOSkin.Champion.ToChampion(), dTOSkin.Price, dTOSkin.Icon, dTOSkin.Image, dTOSkin.Description);
+        }
+    }
+}
--- Sources/DTOLol/DTOSkin.cs
+++ Sources/DTOLol/DTOSkin.cs
@@ -1,7 +1,37 @@
-﻿namespace DTOLol
+﻿using Model;
+
+namespace DTOLol
 {
     public class DTOSkin
     {
-        
+        public String Name
+        {
+            get; set;
+        }
+
+        public String Description
+        {
+            get; set;
+        }
+
+        public float Price
+        {
+            get; set;
+        }
+
+        public DTOChampion Champion
+        {
+            get; set;
+        }
+
+        public String Icon
+        {
+            get; set;
+        }
+
+        public String Image
+        {
+            get; set;
+        }
     }
 }
