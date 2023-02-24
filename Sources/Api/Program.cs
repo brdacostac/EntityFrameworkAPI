0bf23b587d4112c93b68043032f45dd4efe02f67
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-02-24 15:27:35 +0100

    ça ne marche pas encore, on va regarder la prochaine fois avec le prof. Swagger dans le containeur

--- Sources/Api/Program.cs
+++ Sources/Api/Program.cs
@@ -16,25 +16,22 @@ builder.Services.AddTransient<IDataManager, StubData>();//un seul pour chque env
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-/*if (app.Environment.IsDevelopment())
-{*/
-app.UseSwagger();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
     app.UseSwaggerUI();
-/*}
+}
 else
 {
-    app.UseSwagger(c =>
-    {
-        c.RouteTemplate = "/ramikhedair-entityframework_api_lol/swagger";
-    });
+    app.UseSwagger();
 
 
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/ramikhedair-entityframework_api_lol/swagger", "API V1");
+        c.SwaggerEndpoint("/ramikhedair-entityframework_api_lol/swagger", "Api V1");
         c.RoutePrefix = "/ramikhedair-entityframework_api_lol/swagger";
     });
-}*/
+}
 
 //app.UseHttpsRedirection();
 
