e9db7a2d36deef252cdd8ce15e62592b074b7b7b
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-02-24 15:09:05 +0100

    Update Swagger api

--- Sources/Api/Program.cs
+++ Sources/Api/Program.cs
@@ -25,14 +25,14 @@ else
 {
     app.UseSwagger(c =>
     {
-        c.RouteTemplate = "/containers/ramikhedair-entityframework_api_lol/swagger";
+        c.RouteTemplate = "/ramikhedair-entityframework_api_lol/swagger";
     });
 
 
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/containers/ramikhedair-entityframework_api_lol/swagger", "API V1");
-        c.RoutePrefix = "/containers/ramikhedair-entityframework_api_lol/swagger";
+        c.SwaggerEndpoint("/ramikhedair-entityframework_api_lol/swagger", "API V1");
+        c.RoutePrefix = "/ramikhedair-entityframework_api_lol/swagger";
     });
 }
 
