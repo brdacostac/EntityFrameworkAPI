daf2d7c679bfe1198c8610d4f6bbeaf496549533
rakhedair <rami.khedair@etu.uca.fr>
2023-03-03 16:33:26 +0100

    the client of champion

--- Sources/ClientApi/Program.cs
+++ Sources/ClientApi/Program.cs
@@ -8,6 +8,15 @@ foreach(DTOChampion champ in await championHttpManager.GetItems(1,0))
 {
     Console.WriteLine(champ.Name);
 }
-DTOChampion champ1 = await championHttpManager.GetItemByName("SQD");
+DTOChampion champ1 = await championHttpManager.GetItemByName("Akali");
+champ1.Image = "qsd";
+champ1.Bio = "qsd";
+champ1.skins = new List<DTOSkin> { new DTOSkin() };
+
+await championHttpManager.DeleteItem(champ1);
+await championHttpManager.AddItem(champ1);
+//DTOChampion champ2 = await championHttpManager.GetItemByName("Akali");
+
 Console.WriteLine(champ1.Name);
+
 Console.ReadLine();
