df48d2988f21f3eccbe540201393ac10b59ac5f3
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-03-06 19:28:21 +0100

    Test Delete Fait Champion_UT

--- Sources/Champion_TestMemory/Champion_UT_In_Memory.cs
+++ Sources/Champion_TestMemory/Champion_UT_In_Memory.cs
@@ -76,5 +76,44 @@ namespace Champion_TestMemory
                 Assert.Equal(1, context.ChampionsSet.Where(n => n.Name.ToLower().Contains(nameToFind)).Count());
             }
         }
+
+        [Fact]
+        public void Delete_Test()
+        {
+            var options = new DbContextOptionsBuilder<ChampionsDbContexte>()
+                .UseInMemoryDatabase(databaseName: "Delete_Test_Database")
+                .Options;
+
+            using (var context = new ChampionsDbContexte(options))
+            {
+
+                ChampionDB gnar = new ChampionDB { Name = "Gnar", Bio = "Teste gnar", Icon = "dzadaz" };
+                ChampionDB vladimir = new ChampionDB { Name = "Vladimir", Bio = "Teste Vlad", Icon = "dzadaz" };
+                ChampionDB corki = new ChampionDB { Name = "Corki", Bio = "Teste Corki", Icon = "dzadaz" };
+
+                context.ChampionsSet.Add(gnar);
+                context.ChampionsSet.Add(vladimir);
+                context.ChampionsSet.Add(corki);
+                context.SaveChanges();
+            }
+
+            using (var context = new ChampionsDbContexte(options))
+            {
+                string nameToFind = "nar";
+                Assert.Equal(1, context.ChampionsSet.Where(n => n.Name.ToLower().Contains(nameToFind)).Count());
+                nameToFind = "vlad";
+                Assert.Equal(1, context.ChampionsSet.Where(n => n.Name.ToLower().Contains(nameToFind)).Count());
+
+                context.ChampionsSet.Remove(context.ChampionsSet.Where(n => n.Name.ToLower().Contains(nameToFind)).First());
+                context.SaveChanges();
+            }
+
+            using (var context = new ChampionsDbContexte(options))
+            {
+                string nameToFind = "vlad";
+                Assert.NotEqual(1, context.ChampionsSet.Where(n => n.Name.ToLower().Contains(nameToFind)).Count());
+            }
+
+        }
     }
 }
