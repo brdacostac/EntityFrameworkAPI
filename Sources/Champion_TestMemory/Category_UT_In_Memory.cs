0df7f413ee3074bfd4afeed044411ecfe191c7de
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-03-18 17:23:13 +0100

    Category_UT methode delete fait en MemberData

--- Sources/Champion_TestMemory/Category_UT_In_Memory.cs
+++ Sources/Champion_TestMemory/Category_UT_In_Memory.cs
@@ -106,8 +106,15 @@ namespace TestEntityUT
             }
         }
 
-        [Fact]
-        public void Delete_Test()
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { "Rune Page 1", 1 };
+            yield return new object[] { "Rune Page 2", 2 };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Delete_Test(string nameToDelete, int expectedCount)
         {
             var options = new DbContextOptionsBuilder<EntityDbContexte>()
                 .UseInMemoryDatabase(databaseName: "Delete_Test_Database")
@@ -115,19 +122,15 @@ namespace TestEntityUT
 
             using (var context = new EntityDbContexte(options))
             {
-
                 RunePagesDb runePage1 = new RunePagesDb { Name = "Rune Page 1" };
                 RunePagesDb runePage2 = new RunePagesDb { Name = "Rune Page 2" };
                 RuneDB rune1 = new RuneDB { Name = "Rune 1", Description = "Test Rune 1", Icon = "icon1.png", Family = RuneFamilyDb.Precision };
                 RuneDB rune2 = new RuneDB { Name = "Rune 2", Description = "Test Rune 2", Icon = "icon2.png", Family = RuneFamilyDb.Domination };
                 RuneDB rune3 = new RuneDB { Name = "Rune 3", Description = "Test Rune 3", Icon = "icon3.png", Family = RuneFamilyDb.Unknown };
-
                 CategoryDicDB category1 = new CategoryDicDB { category = CategoryDb.Major, runePage = runePage1, rune = rune1 };
                 CategoryDicDB category2 = new CategoryDicDB { category = CategoryDb.Minor1, runePage = runePage1, rune = rune2 };
                 CategoryDicDB category3 = new CategoryDicDB { category = CategoryDb.Minor2, runePage = runePage1, rune = rune3 };
                 CategoryDicDB category4 = new CategoryDicDB { category = CategoryDb.OtherMinor1, runePage = runePage2, rune = rune1 };
-
-
                 context.RunePagesSet.Add(runePage1);
                 context.RunePagesSet.Add(runePage2);
                 context.RunesSet.Add(rune1);
@@ -137,23 +140,12 @@ namespace TestEntityUT
                 context.CategoryRunePageSet.Add(category2);
                 context.CategoryRunePageSet.Add(category3);
                 context.CategoryRunePageSet.Add(category4);
-
-                context.SaveChanges();
-            }
-
-            using (var context = new EntityDbContexte(options))
-            {
-                string nameToFind = "page 1";
-                Assert.Equal(1, context.RunePagesSet.Where(n => n.Name.ToLower().Contains(nameToFind)).Count());
-                context.RunePagesSet.Remove(context.RunePagesSet.Where(n => n.Name.ToLower().Contains(nameToFind)).First());
                 context.SaveChanges();
 
-            }
+                context.RunePagesSet.Remove(context.RunePagesSet.Where(n => n.Name.ToLower().Contains(nameToDelete.ToLower())).First());
+                context.SaveChanges();
 
-            using (var context = new EntityDbContexte(options))
-            {
-                string nameToFind = "Rune Page 1";
-                Assert.NotEqual(1, context.RunePagesSet.Where(n => n.Name.ToLower().Contains(nameToFind)).Count());
+                Assert.Equal(expectedCount, context.RunePagesSet.Count());
             }
         }
 
